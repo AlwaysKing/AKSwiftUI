d4a4660e318980fa85989a33fd4f36763f917f2e
alwaysking <alwaysking0322@hotmail.com>
2025-03-06 18:22:00 +0800

    新增 stepScrollVIew

--- Sources/AKSwiftUI/AKSUStepScrollview.swift
+++ Sources/AKSwiftUI/AKSUStepScrollview.swift
@@ -2,7 +2,7 @@
 //  AKSUStepScrollview.swift
 //  AKSwiftUI
 //
-//  Created by cnsinda on 2025/3/5.
+//  Created by AlwaysKing on 2025/3/5.
 //
 
 import SwiftUI
@@ -29,14 +29,18 @@ public struct AKSUStepScrollview<K, V: View>: View {
     let keys: [K]
     let direction: AKSUStepScrollviewDirection
     let alignment: AKSUStepScrollviewAlignment
+    let btnSmartHidden: Bool
     let crossShrink: Bool
     let contentBuilder: (K) -> V
 
-    public init(keys: [K], direction: AKSUStepScrollviewDirection = .horizontal, alignment: AKSUStepScrollviewAlignment = .center, crossShrink: Bool = true, contentBuilder: @escaping (K) -> V) {
+    var btnBuilder: ((Bool, CGFloat, @escaping () -> Void) -> AnyView)? = nil
+
+    public init(keys: [K], direction: AKSUStepScrollviewDirection = .horizontal, alignment: AKSUStepScrollviewAlignment = .center, crossShrink: Bool = true, btnSmartHidden: Bool = false, contentBuilder: @escaping (K) -> V) {
         self.keys = keys
         self.direction = direction
         self.crossShrink = crossShrink
         self.contentBuilder = contentBuilder
+        self.btnSmartHidden = btnSmartHidden
         self.alignment = alignment
     }
 
@@ -47,24 +51,30 @@ public struct AKSUStepScrollview<K, V: View>: View {
                     if alignment != .head {
                         Spacer()
                     }
-                    if showButton() {
-                        ZStack {
-                            Image(systemName: direction == .horizontal ? "chevron.compact.left" : "chevron.compact.up")
-                                .foregroundStyle(.white)
-                        }
-                        .frame(width: direction == .horizontal ? 20 : 40, height: direction == .horizontal ? 40 : 20)
-                        .background(leftHovering ? .aksuDYGrayMask : .aksuDYGrayBG)
-                        .cornerRadius(8)
-                        .onHover {
-                            leftHovering = $0
-                            if $0 {
-                                NSCursor.pointingHand.push()
-                            } else {
-                                NSCursor.pop()
+                    if showPreButton() {
+                        if let btnBuilder = btnBuilder {
+                            btnBuilder(true, crossSize()) {
+                                scrollProxy.scrollTo(max((showIndex.min() ?? 0) - 1, 0), anchor: direction == .horizontal ? .leading : .top)
                             }
-                        }
-                        .onTapGesture {
-                            scrollProxy.scrollTo(max((showIndex.min() ?? 0) - 1, 0), anchor: direction == .horizontal ? .leading : .top)
+                        } else {
+                            ZStack {
+                                Image(systemName: direction == .horizontal ? "chevron.compact.left" : "chevron.compact.up")
+                                    .foregroundStyle(.white)
+                            }
+                            .frame(width: direction == .horizontal ? 20 : 40, height: direction == .horizontal ? 40 : 20)
+                            .background(leftHovering ? .aksuDYGrayMask : .aksuDYGrayBG)
+                            .cornerRadius(8)
+                            .onHover {
+                                leftHovering = $0
+                                if $0 {
+                                    NSCursor.pointingHand.push()
+                                } else {
+                                    NSCursor.pop()
+                                }
+                            }
+                            .onTapGesture {
+                                scrollProxy.scrollTo(max((showIndex.min() ?? 0) - 1, 0), anchor: direction == .horizontal ? .leading : .top)
+                            }
                         }
                     }
 
@@ -74,11 +84,11 @@ public struct AKSUStepScrollview<K, V: View>: View {
                                 no in
                                 contentBuilder(keys[no])
                                     .id(no)
-                                    .onAppear {
-                                        showIndex.append(no)
-                                    }
                                     .onDisappear {
                                         showIndex.removeAll { $0 == no }
+                                    }
+                                    .onAppear {
+                                        showIndex.append(no)
                                     }
                             }
                         }
@@ -97,24 +107,30 @@ public struct AKSUStepScrollview<K, V: View>: View {
                     }
                     .frame(maxWidth: width(), maxHeight: height())
 
-                    if showButton() {
-                        ZStack {
-                            Image(systemName: direction == .horizontal ? "chevron.compact.right" : "chevron.compact.down")
-                                .foregroundStyle(.white)
-                        }
-                        .frame(width: direction == .horizontal ? 20 : 40, height: direction == .horizontal ? 40 : 20)
-                        .background(rightHovering ? .aksuDYGrayMask : .aksuDYGrayBG)
-                        .cornerRadius(8)
-                        .onHover {
-                            rightHovering = $0
-                            if $0 {
-                                NSCursor.pointingHand.push()
-                            } else {
-                                NSCursor.pop()
+                    if showNextButton() {
+                        if let btnBuilder = btnBuilder {
+                            btnBuilder(false, crossSize()) {
+                                scrollProxy.scrollTo(min((showIndex.max() ?? 0) + 1, keys.count - 1), anchor: direction == .horizontal ? .trailing : .bottom)
                             }
-                        }
-                        .onTapGesture {
-                            scrollProxy.scrollTo(min((showIndex.max() ?? 0) + 1, keys.count - 1), anchor: direction == .horizontal ? .trailing : .bottom)
+                        } else {
+                            ZStack {
+                                Image(systemName: direction == .horizontal ? "chevron.compact.right" : "chevron.compact.down")
+                                    .foregroundStyle(.white)
+                            }
+                            .frame(width: direction == .horizontal ? 20 : 40, height: direction == .horizontal ? 40 : 20)
+                            .background(rightHovering ? .aksuDYGrayMask : .aksuDYGrayBG)
+                            .cornerRadius(8)
+                            .onHover {
+                                rightHovering = $0
+                                if $0 {
+                                    NSCursor.pointingHand.push()
+                                } else {
+                                    NSCursor.pop()
+                                }
+                            }
+                            .onTapGesture {
+                                scrollProxy.scrollTo(min((showIndex.max() ?? 0) + 1, keys.count - 1), anchor: direction == .horizontal ? .trailing : .bottom)
+                            }
                         }
                     }
 
@@ -160,6 +176,32 @@ public struct AKSUStepScrollview<K, V: View>: View {
         return nil
     }
 
+    func crossSize() -> CGFloat {
+        return direction == .horizontal ? contentSize.height : contentSize.width
+    }
+
+    func showPreButton() -> Bool {
+        if !showButton() {
+            return false
+        }
+
+        if btnSmartHidden && showIndex.min() == 0 {
+            return false
+        }
+        return true
+    }
+
+    func showNextButton() -> Bool {
+        if !showButton() {
+            return false
+        }
+
+        if btnSmartHidden && showIndex.max() == keys.count - 1 {
+            return false
+        }
+        return true
+    }
+
     func showButton() -> Bool {
         if direction == .horizontal {
             if contentSize.width > boardSize.width {
@@ -171,6 +213,14 @@ public struct AKSUStepScrollview<K, V: View>: View {
             }
         }
         return false
+    }
+
+    public func setBtnViewBuilder<btn: View>(_ builder: @escaping (_ head: Bool, _ direction: AKSUStepScrollviewDirection, _ size: CGFloat, _ click: @escaping () -> Void) -> btn) -> AKSUStepScrollview {
+        var rv = self
+        rv.btnBuilder = { head, size, click in
+            AnyView(builder(head, rv.direction, size, click))
+        }
+        return rv
     }
 }
 
@@ -185,7 +235,7 @@ struct AKSUStepScrollview_Previews: PreviewProvider {
 
 struct AKSUStepScrollviewPreviewsView: View {
     @State var count: Int = 20
-    @State var direction: AKSUStepScrollviewDirection = .vertical
+    @State var direction: AKSUStepScrollviewDirection = .horizontal
     @State var alignment: AKSUStepScrollviewAlignment = .center
 
     var body: some View {
@@ -207,23 +257,67 @@ struct AKSUStepScrollviewPreviewsView: View {
                 Text("垂直").AKSUSegmentTag(index: .vertical)
                 Text("水平").AKSUSegmentTag(index: .horizontal)
             }
-            
+
             AKSUSegment(selected: $alignment) {
                 Text("head").AKSUSegmentTag(index: .head)
                 Text("center").AKSUSegmentTag(index: .center)
                 Text("end").AKSUSegmentTag(index: .end)
             }
             .padding(.bottom, direction == .vertical ? 10 : 100)
-            
-            ZStack {
-                AKSUStepScrollview(keys: Array(0 ... count), direction: direction, alignment: alignment) { key in
-                    Text("\(key)")
-                        .padding()
-                        .foregroundStyle(.white)
-                        .background(.green)
-                        .cornerRadius(4)
-                }
+
+            AKSUStepScrollview(keys: Array(0 ... count), direction: direction, alignment: alignment) { key in
+                Text("\(key)")
+                    .padding()
+                    .foregroundStyle(.white)
+                    .background(.green)
+                    .cornerRadius(4)
             }
+
+            AKSUStepScrollview(keys: Array(0 ... count), direction: direction, alignment: alignment, btnSmartHidden: true) { key in
+                Text("\(key)")
+                    .padding()
+                    .foregroundStyle(.white)
+                    .background(.green)
+                    .cornerRadius(4)
+            }
+
+            AKSUStepScrollview(keys: Array(0 ... count), direction: direction, alignment: alignment, btnSmartHidden: true) { key in
+                Text("\(key)")
+                    .padding()
+                    .foregroundStyle(.white)
+                    .background(.green)
+                    .cornerRadius(4)
+            }
+            .setBtnViewBuilder(ClickBtn)
+        }
+    }
+
+    @State var leftHovering: Bool = false
+    @State var rightHovering: Bool = false
+
+    @ViewBuilder
+    func ClickBtn(head: Bool, direction: AKSUStepScrollviewDirection, size: CGFloat, click: @escaping () -> Void) -> some View {
+        ZStack {
+            if head {
+                Image(systemName: direction == .horizontal ? "chevron.compact.left" : "chevron.compact.up")
+                   
+            } else {
+                Image(systemName: direction == .horizontal ? "chevron.compact.right" : "chevron.compact.down")
+                   
+            }
+        }
+        .frame(width: direction == .horizontal ? 10 : size, height: direction == .vertical ? 10 : size)
+        .background(.white.opacity(0.01))
+        .onHover {
+            if head {
+                leftHovering = $0
+            } else {
+                rightHovering = $0
+            }
+        }
+        .opacity(head ? (leftHovering ? 0.5 : 1) : (rightHovering ? 0.5 : 1))
+        .onTapGesture {
+            click()
         }
     }
 }
