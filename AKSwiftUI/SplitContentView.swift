ceb9314fcb5c4312ef15a576b91273f91486c8e1
alwaysking <alwaysking0322@hotmail.com>
2026-05-24 23:50:57 +0800

    no message

--- AKSwiftUI/SplitContentView.swift
+++ AKSwiftUI/SplitContentView.swift
@@ -19,7 +19,7 @@ struct SplitContentView: View {
     @State var g: CGFloat = 0.0
     @State var b: CGFloat = 0.0
 
-    @State var selected: String = "Table"
+    @State var selected: String = "Color"
 
     let menu: [String: [String]] = [
         "主题": ["Color", "Font"],
